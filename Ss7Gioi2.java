93533a724c676842a9e251e01dc645d940c2897a
Nguyễn Việt Dương <duong74dcht22210@st.utt.edu.vn>
2026-05-04 09:15:05 +0700

    Gioi2

---
+++ Ss7Gioi2.java
@@ -0,0 +1,11 @@
+public class Ss7Gioi2 {
+    public String title;
+    public String author;
+    public double price;
+
+    public void printInfo() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Price: " + price);
+    }
+}
